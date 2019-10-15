c698f8f1dec4790afe77c93cc5a14b56e7181532
Julian Camp <38470746+jcamp5@users.noreply.github.com>
2019-10-15 01:16:37 -0400

    Changed gitignore to remove client access token and DB credentials

--- Chart_Lyric_Update.py
+++
@@ -1,8 +0,0 @@
-import lyricsgenius
-
-
-genius = lyricsgenius.Genius('bFSy9jEEXEe758Kf82Tpe-pUPb5A9Gs8r4F_YKn6BehF4u6sphLMEaUprO4bOt__')
-song = genius.search_song('Truth Hurts', 'Lizzo')
-test = song.lyrics
-
-print(test)
